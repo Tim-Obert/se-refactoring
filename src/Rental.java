863cd9fa30dcaf6873f7ee63cd4979a5c4e2b27b
Tim Obert <tim-obert@web.de>
2020-05-14 14:07:47 +0200

    Replace Conditional with Polymorphism

--- src/Rental.java
+++ src/Rental.java
@@ -14,30 +14,11 @@ class Rental {
 
 
     double getCharge() {
-        double amount = 0;
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                amount += 2;
-                if (getDaysRented() > 2)
-                    amount += (getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                amount += getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                amount += 1.5;
-                if (getDaysRented() > 3)
-                    amount += (getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return amount;
+        return this.movie.getCharge(this.daysRented);
     }
 
     int getFrequentRenterPoints() {
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
-            return 2;
-        else
-            return 1;
+        return this.movie.getFrequentRenterPoints(this.daysRented);
     }
 
 }
